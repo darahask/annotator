7586d4abaf2b6850412f8d9f063b7dfd7148a742
darahask <darahask12@gmail.com>
2022-02-21 17:50:25 +0530

    add annotations list

--- src/components/Annotationcanvas.js
+++ src/components/Annotationcanvas.js
@@ -1,19 +1,20 @@
 import { useEffect, useRef } from "react";
 import "../styles/popup.css";
 
-const Annotationcanvas = () => {
+const Annotationcanvas = ({ data }) => {
     let ref = useRef();
+    let { annotations, setAnnotations } = data;
 
     useEffect(() => {
         let canvas = ref.current;
         let ctx = canvas.getContext("2d");
-        var shapes = [];
+        var shapes = annotations;
         var tempShape = {};
         var isDragging = false;
 
         let image = new Image();
         image.src =
-            "https://cdn.pixabay.com/photo/2021/08/18/18/17/office-6556228_1280.png";
+            "https://res.cloudinary.com/practicaldev/image/fetch/s--_7arERV2--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/ikkj6js77uyo7m0rd1ud.jpeg";
         image.onload = () => {
             canvas.width = image.width;
             canvas.height = image.height;
@@ -32,22 +33,28 @@ const Annotationcanvas = () => {
         }
         var offsetX, offsetY;
         reOffset();
-        window.onscroll = function (e) {
-            reOffset();
-        };
-        window.onresize = function (e) {
-            reOffset();
-        };
-        canvas.onresize = function (e) {
-            reOffset();
-        };
 
         canvas.onmousedown = handleMouseDown;
         canvas.onmousemove = handleMouseMove;
         canvas.onmouseup = handleMouseUp;
         canvas.onmouseout = handleMouseOut;
 
-        function collides(rects, x, y) {
+        function drawAll() {
+            let cw = canvas.width;
+            let ch = canvas.height;
+            ctx.clearRect(0, 0, cw, ch);
+            for (var i = 0; i < shapes.length; i++) {
+                var shape = shapes[i];
+                if (shape.type === "image") {
+                    ctx.drawImage(shape.img, 0, 0);
+                } else {
+                    ctx.lineWidth = 2;
+                    ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+                }
+            }
+        }
+
+        function collides(x, y) {
             var isCollision = false;
             for (var i = 0, len = shapes.length; i < len; i++) {
                 if (shapes[i].type === "rectangle") {
@@ -56,7 +63,7 @@ const Annotationcanvas = () => {
                     var top = shapes[i].y,
                         bottom = shapes[i].y + shapes[i].height;
                     if (right >= x && left <= x && bottom >= y && top <= y) {
-                        isCollision = shapes[i];
+                        isCollision = i;
                     }
                 }
             }
@@ -65,7 +72,7 @@ const Annotationcanvas = () => {
 
         canvas.addEventListener("click", (e) => {
             console.log("click: " + e.offsetX + "/" + e.offsetY);
-            var rect = collides(shapes, e.offsetX, e.offsetY);
+            var rect = collides(e.offsetX, e.offsetY);
             if (rect) {
                 launchPopup(rect, offsetX, offsetY);
             } else {
@@ -95,7 +102,7 @@ const Annotationcanvas = () => {
             e.preventDefault();
             e.stopPropagation();
             // the drag is over -- clear the isDragging flag
-            shapes.push(tempShape);
+            if(tempShape.width || tempShape.height)shapes.push(tempShape);
             tempShape = {};
             isDragging = false;
         }
@@ -110,7 +117,7 @@ const Annotationcanvas = () => {
             e.preventDefault();
             e.stopPropagation();
             // the drag is over -- clear the isDragging flag
-            shapes.push(tempShape);
+            if(tempShape.width || tempShape.height)shapes.push(tempShape);
             tempShape = {};
             isDragging = false;
         }
@@ -136,7 +143,41 @@ const Annotationcanvas = () => {
             tempShape.height = dy;
         }
 
+        //pop up handlers
+        let selectedRectangle;
         let popup = document.getElementById("pop");
+        let popname = document.getElementById("pop-name");
+        let popcheck = document.getElementById("pop-check");
+
+        popname.onchange = (e) => {
+            e.preventDefault();
+            shapes[selectedRectangle].name = e.target.value;
+        };
+
+        popcheck.onclick = (e) => {
+            shapes[selectedRectangle].isAntiPattern = e.target.checked;
+        };
+
+        document.getElementById("pop-save").onclick = (e) => {
+            e.preventDefault();
+            let lannotations = shapes.filter((shape) => {
+                return (shape.type === "rectangle")? true : false;
+            });
+            setAnnotations(lannotations);
+            popup.style.display = "none";
+        };
+
+        document.getElementById("pop-delete").onclick = (e) => {
+            e.preventDefault();
+            shapes.splice(selectedRectangle, 1);
+            drawAll();
+            let lannotations = shapes.filter((shape) => {
+                return (shape.type === "rectangle")? true : false;
+            });
+            setAnnotations(lannotations);
+            popup.style.display = "none";
+        };
+
         document.getElementById("pop-close").onclick = (e) => {
             e.preventDefault();
             popup.style.top = 0;
@@ -144,39 +185,51 @@ const Annotationcanvas = () => {
             popup.style.display = "none";
         };
 
-        function launchPopup(shape, offx, offy) {
+        function launchPopup(i, offx, offy) {
+            selectedRectangle = i;
+            let shape = shapes[i];
             popup.style.display = "block";
             popup.style.top = offy + shape.y + shape.height + "px";
             popup.style.left = offx + shape.x + shape.width + "px";
-        }
-
-        function drawAll() {
-            let cw = canvas.width;
-            let ch = canvas.height;
-            ctx.clearRect(0, 0, cw, ch);
-            for (var i = 0; i < shapes.length; i++) {
-                var shape = shapes[i];
-                if (shape.type === "image") {
-                    ctx.drawImage(shape.img, 0, 0);
-                } else {
-                    ctx.lineWidth = 2;
-                    ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
-                }
-            }
+            console.log(shapes.name,shape.isAntiPattern)
+            popname.value = shape.name ?? "";
+            popcheck.checked = shape.isAntiPattern ?? false
         }
     }, []);
 
     return (
-        <>
+        <div>
             <div id="pop" className="canvas-popup">
-                <input type="text"></input>
-                <br></br>
-                <input type="checkbox" id="pop-check"></input>
-                <label htmlFor="pop-check">Mark as antipattern</label>
-                <br></br>
-                <button>Save </button>
-                <button>Delete </button>
-                <button id="pop-close">Close</button>
+                <div className="mb-3">
+                    <label htmlFor="pop-name" className="form-label">
+                        Email address
+                    </label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="pop-name"
+                        aria-describedby="emailHelp"
+                    ></input>
+                </div>
+                <div className="mb-3 form-check">
+                    <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="pop-check"
+                    ></input>
+                    <label className="form-check-label" htmlFor="pop-check">
+                        Mark as anti-pattern
+                    </label>
+                </div>
+                <button id="pop-save" className="btn btn-success">
+                    Save
+                </button>
+                <button id="pop-delete" className="btn btn-danger">
+                    Delete
+                </button>
+                <button id="pop-close" className="btn btn-primary">
+                    Close
+                </button>
             </div>
             <div
                 style={{
@@ -184,13 +237,13 @@ const Annotationcanvas = () => {
                     width: "100%",
                     height: "800px",
                     overflow: "scroll",
-                    border: "2px solid black",
+                    border: "1px solid black",
                     margin: "15px",
                 }}
             >
                 <canvas ref={ref}></canvas>
             </div>
-        </>
+        </div>
     );
 };
 
--- src/screens/Document.js
+++ src/screens/Document.js
@@ -1,15 +1,22 @@
 import { connect } from "react-redux";
 import Managemodel from "../components/Managemodel";
 import Annotationcanvas from "../components/Annotationcanvas";
+import Annotationlist from "../components/Annotationlist";
+import { useState } from "react";
 
 const Document = (props) => {
+
+    const [annotations, setAnnotations] = useState([])
+
     return (
         <div>
-            <div className="row justify-content-center">
+            <div className="row justify-content-around">
                 <div className="col-md-8">
-                    <Annotationcanvas />
+                    <Annotationcanvas data={{annotations,setAnnotations}} />
                 </div>
-                <div className="col-md-3"></div>
+                <div className="col-md-3">
+                    <Annotationlist data={{annotations,setAnnotations}}/>
+                </div>
             </div>
             <Managemodel />
         </div>
